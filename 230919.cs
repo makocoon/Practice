4b99d70d70e60e4fb0659b3e4a9a6849c74ce96e
naoto akashi <aksh0402@outlook.jp>
2023-10-01 09:55:10 +0900

    0919コメント

--- 230919.cs
+++ 230919.cs
@@ -8,10 +8,10 @@ namespace rensyu
         //BMI25以上なら、肥満。25未満なら肥満でない。
         static void Main(string[] args)
         {
-            //// 命名規則もう一度見直したほうが良いかもです。
             //// Parse失敗したら落ちません？TryParseしてエラー処理したほうがいいです。
             
             Console.WriteLine("身長を入力してください。[cm]");
+            ////　↓も消してください。L19も同様。
             //double height = double.Parse(Console.ReadLine());
             double.TryParse(Console.ReadLine(), out double height);
 
@@ -22,6 +22,8 @@ namespace rensyu
             double bmi = weight / (Math.Pow((height / 100), 2));
             Console.WriteLine("BMIは、" + bmi.ToString("F2") + "です。");
 
+            //// bmi >= 25が何さしてるのか、下のコード見ないと、
+            //// わからないので、一時変数で受けたほうがいいです。
             if (bmi >= 25)
             {
                 Console.WriteLine("あなたは肥満です。");
