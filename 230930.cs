4963a0f1b86cd83589e24900ffb6e0c3dc19ebb5
Makiko Sho <makochip@outlook.jp>
2023-10-01 09:54:30 +0900

    半径から体積をもとめる

---
+++ 230930.cs
@@ -0,0 +1,17 @@
+﻿//問題
+//入力された半径（cm）をもつ球の体積を単位ccで表示せよ。
+//円周率は3.14とする
+
+namespace rensyu
+{
+    internal class rensyu
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("半径は？");
+            double.TryParse(Console.ReadLine(), out double radius);
+            double volume = 4 * Math.PI * Math.Pow(radius, 3) / 3;
+            Console.WriteLine($"球の体積は{volume:F2}です。");
+        }
+    }
+}
